7ac5951e7ed633cd6472ca4bc592f2de92d152ae
Sarah Schulte <git@sarah.engineer>
2026-09-13 13:03:39 +0000

    cleanup comments

--- net/interop.h
+++ net/interop.h
@@ -59,36 +59,3 @@ typedef struct __attribute__((packed)) {
   unsigned long long id;
   ClientMsgUpdate stuff;
 } ServerMsgUpdatePlayer;
-
-/*
-////////////////////////////////////////////////////////////////////////////////
-// Defining a type like this:
-
-  typedef struct ServerMsg {
-    ServerMsgTag tag;
-    union {
-      ServerMsgConnected connected;
-      ServerMsgDisconnected disconnected;
-      ServerMsgUpdatePlayer updatePlayer;
-    } data;
-  } ServerMsg;
-
-// Would allow this in testclient.c `handle_message`:
-
-  ServerMsg *serverMsg = (ServerMsg *)msg;
-  switch (serverMsg->tag) {
-    case ServerUpdatePlayer: {
-      ServerMsgUpdatePlayer *update = serverMsg->data.updatePlayer;
-  }
-
-////////////////////////////////////////////////////////////////////////////////
-// could also do this:
-
-ServerMsgTag tag = *((ServerMsgTag*)msg);
-void *data = msg + sizeof(ServerMsgTag);
-
-  case:
-    ServerMsgUpdatePlayer *update = (ServerMsgUpdatePlayer*)data;
-
-////////////////////////////////////////////////////////////////////////////////
-*/
--- net/server.c
+++ net/server.c
@@ -1,25 +1,3 @@
-/*
- * Copyright 2018 Markus Lindelöw
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files(the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #define TINYCSOCKET_IMPLEMENTATION
 #include "tinycsocket.h"
 #include "interop.h"
@@ -29,6 +7,7 @@
 
 #define MAX_CONNECTIONS 1024
 #define RECV_BUFSIZE 1024
+
 typedef struct UserData {
   unsigned long long id;
   size_t pidx;
@@ -51,9 +30,6 @@ PlayerData pdata[MAX_CONNECTIONS] = {};
 #else
 #define LOG_DEBUG(...) __VA_ARGS__;
 #endif
-
-// i think i separate fmt usually so i can do e.g.
-// LOG_DEBUG(fmt, ...) printf("[debug]" fmt, __VA_ARGS__)
 
 static int show_error(const char* error_text)
 {
